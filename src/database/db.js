457720fd569229b39a881f3d24271617940fc1a9
Bharath <albharath.1305@gmail.com>
2020-02-15 16:45:55 +0530

    use env variable

--- src/database/db.js
+++ src/database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const mongoURL = "mongodb://127.0.0.1:27017/bitflix-api";
+const mongoURL = process.env.MONGODB_URL;
 
 try {
     mongoose.connect(mongoURL, {
